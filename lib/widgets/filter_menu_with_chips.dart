5c1e6fd3d61ff2aa79e5217e2c40f8cf0471156b
“kaixintoy8” <toybegum@gmail.com>
2025-07-14 17:15:59 +0300

    second commit

--- lib/widgets/filter_menu_with_chips.dart
+++ lib/widgets/filter_menu_with_chips.dart
@@ -13,8 +13,68 @@ class _FilterMenuWithChipsState extends State<FilterMenuWithChips> {
     "Beden": ["S", "M", "L"],
     "Marka": ["Nike", "Adidas", "Puma"],
   };
+  final Set<String> selectedFilters = {};
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Kategori Seçimi"),
+        backgroundColor: Colors.purple,
+        foregroundColor: Colors.white,
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(12),
+        children: [
+          Text(
+            "Filtreler",
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 12),
+          //ExpansionTile Liste
+          ...filters.entries.map((entry) {
+            final kategori = entry.key;
+            final secenekler = entry.value;
+            return ExpansionTile(
+              title: Text(kategori),
+              children: secenekler.map((secenek) {
+                return CheckboxListTile(
+                  title: Text(secenek),
+                  value: selectedFilters.contains(secenek),
+                  onChanged: (val) {
+                    setState(() {
+                      if (val == true) {
+                        selectedFilters.add(secenek);
+                      } else {
+                        selectedFilters.remove(secenek);
+                      }
+                    });
+                  },
+                );
+              }).toList(),
+            );
+          }).toList(),
+          Divider(),
+          Text(
+            "Seçilen Filtreler",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+          ),
+          SizedBox(height: 8),
+          Wrap(
+            spacing: 8,
+            runSpacing: 8,
+            children: selectedFilters.map((filter) {
+              return Chip(
+                label: Text(filter),
+                onDeleted: () {
+                  setState(() {
+                    selectedFilters.remove(filter);
+                  });
+                },
+              );
+            }).toList(),
+          ),
+        ],
+      ),
+    );
   }
 }
